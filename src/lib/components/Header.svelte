1b97e06555a47a6c2bdde6d9b47dc059ed3f644e
pierre lespingal <pierre.lespingal@gmail.com>
2023-10-28 16:44:22 +0200

    feat: Update favicon & header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,6 +2,8 @@
   import Notes from "./Notes.svelte";
 </script>
 
-<header class="w-full pt-4 text-center text-2xl md:text-4xl">
-  <Notes><span class="text-4xl text-stone-400 md:text-6xl">C</span>ustom</Notes>
-</header>
+<Notes>
+  <header class="w-full pt-4 text-center text-2xl text-blue-400 md:text-4xl">
+    <span class="text-4xl text-stone-200 md:text-6xl">C</span><span>ustom</span>
+  </header>
+</Notes>
