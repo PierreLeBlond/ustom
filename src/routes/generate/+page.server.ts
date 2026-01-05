3c79dfa0cc809e8b3ed5b277475c433a1d5428ec
PierreLeBlond <pierre.lespingal@gmail.com>
2026-01-05 13:22:44 +0100

    fix: Fix sveltekit superform schema

--- src/routes/generate/+page.server.ts
+++ src/routes/generate/+page.server.ts
@@ -3,7 +3,7 @@ import { encrypt } from "$lib/crypto/crypto";
 import { fail } from "@sveltejs/kit";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
-import { z } from "zod";
+import { z } from "zod/v3";
 
 const schema = z.object({
   word: z
